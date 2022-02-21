6c3d4cd017605509944583695f7f1bc7e436cfc7
OmarAbdiAli <omarabdi.ali@se.experis.com>
2022-02-21 09:24:17 +0100

    fix: setup project

---
+++ src/main/java/noroff/assignment_6/data_access_and_display/DataAccessAndDisplayApplication.java
@@ -0,0 +1,13 @@
+package noroff.assignment_6.data_access_and_display;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAccessAndDisplayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataAccessAndDisplayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/noroff/assignment_6/data_access_and_display/DataAccessAndDisplayApplicationTests.java
@@ -0,0 +1,13 @@
+package noroff.assignment_6.data_access_and_display;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAccessAndDisplayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
