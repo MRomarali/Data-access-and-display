73be4ee80471a7370c7e001f86d0aebc9033a51b
Alex On <shuhia.on@gmail.com>
2022-02-24 16:27:26 +0100

    feat(docker): add docker file

--- src/main/java/noroff/assignment_6/data_access_and_display/data_access/ConnectionFactory.java
+++ src/main/java/noroff/assignment_6/data_access_and_display/data_access/ConnectionFactory.java
@@ -5,21 +5,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-    // Pull a file from the resource folder: Northwind_small.sqlite
-    static final String URL = "jdbc:sqlite:src/main/resources/data.sqlite";
+
 
     static public Connection getConnection() {
-        Connection connection = null;
         try {
-            return connection = DriverManager.getConnection(URL);
+            return DriverManager.getConnection("jdbc:sqlite::resource:data.sqlite");
         } catch (SQLException e) {
             e.printStackTrace();
+            return null;
         }
-        try {
-            return connection = DriverManager.getConnection("jdbc:sqlite::resource:data.sqlite");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return connection;
     }
 }
